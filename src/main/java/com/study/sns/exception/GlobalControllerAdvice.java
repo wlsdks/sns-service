52e2d5bc61e7d74e7c323998899f3daf07a035e7
jinan <wlsdks12@naver.com>
2023-06-26 23:20:41 +0900

    1. 예외처리 수정 및 추가

--- src/main/java/com/study/sns/exception/GlobalControllerAdvice.java
+++ src/main/java/com/study/sns/exception/GlobalControllerAdvice.java
@@ -2,6 +2,7 @@ package com.study.sns.exception;
 
 import com.study.sns.controller.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,4 +19,10 @@ public class GlobalControllerAdvice {
                 .body(Response.error(e.getErrorCode().name()));
     }
 
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> applicationHandler(RuntimeException e) {
+        log.error("Error occurs {}", e.toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Response.error(ErrorCode.INTERNAL_SERVER_ERROR.name()));
+    }
 }
--- src/main/java/com/study/sns/exception/SnsApplicationException.java
+++ src/main/java/com/study/sns/exception/SnsApplicationException.java
@@ -10,12 +10,18 @@ public class SnsApplicationException extends RuntimeException {
     private ErrorCode errorCode;
     private String message;
 
+    public SnsApplicationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = null;
+    }
+
     @Override
     public String getMessage() {
         if (message == null) {
             return errorCode.getMessage();
         }
 
-        return String.format("%s. %s", errorCode, getMessage(), message);
+        return String.format("%s. %s", errorCode.getMessage(), message);
     }
+
 }
