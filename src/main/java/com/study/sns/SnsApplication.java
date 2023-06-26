5c14545624f5874c5e445c95a612b9d3460249aa
jinan <wlsdks12@naver.com>
2023-06-26 23:19:23 +0900

    1. postgreSQL설정으로 springboot main코드 설정변경

--- src/main/java/com/study/sns/SnsApplication.java
+++ src/main/java/com/study/sns/SnsApplication.java
@@ -2,9 +2,8 @@ package com.study.sns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SnsApplication {
 
 	public static void main(String[] args) {
