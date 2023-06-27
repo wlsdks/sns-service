415be66ce4e0dcf2ecdbd6d32e0c8e5313c9910d
jinan <wlsdks12@naver.com>
2023-06-27 14:42:35 +0900

    1. 에러코드 추가

--- src/main/java/com/study/sns/exception/ErrorCode.java
+++ src/main/java/com/study/sns/exception/ErrorCode.java
@@ -9,15 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
-
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
-
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
-
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is invalid"),
-
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
-
     ;
 
     private HttpStatus status;
